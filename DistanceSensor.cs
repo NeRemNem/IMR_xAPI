dd5a7731d49ece001aea44f3ec39fd625b15d246
SASARA <hhc940419@gmail.com>
2020-11-16 19:17:20 +0900

    거리측정 관련 코드 추가

---
+++ DistanceSensor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DistanceSensor : MonoBehaviour
+{
+    [SerializeField] 
+    private GameObject _gizmo; 
+    [SerializeField] 
+    private GameObject _start;
+    [SerializeField] 
+    private GameObject _end;
+    private RaycastHit _hitInfo;
+
+    private bool _is_active = true;
+    private int _layer_mask;
+    
+    public UnityEvent ray_hit_event;
+    public List<float> distance_list;
+
+    private void Awake()
+    {
+        ray_hit_event = new UnityEvent();
+        distance_list = new List<float>();
+    }
+
+    void Start()
+    {
+        _gizmo = Instantiate(_gizmo);
+        _gizmo.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        _is_active = true;
+        _layer_mask = 1 << LayerMask.NameToLayer("Wheel");
+    }
+
+    void Update()
+    {
+        if(_is_active)
+        {   
+            if (Physics.Raycast(_start.transform.position, _end.transform.position - _start.transform.position
+                ,out _hitInfo,100f,_layer_mask))
+            {
+                if (_hitInfo.collider.CompareTag("LeftWheel"))
+                {
+                    _is_active = false;
+                    distance_list.Add(GetDistanceToWheelByRaycast(_hitInfo));
+                    ActivateGizmo();
+                    ray_hit_event.Invoke();
+                    this.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        _is_active = false;
+    }
+
+    private void ActivateGizmo()
+    {
+        _gizmo.SetActive(true);
+        _gizmo.transform.position = _hitInfo.collider.transform.position;
+    }
+
+    private float GetDistanceToWheelByRaycast(RaycastHit hit)
+    {
+        Vector3 from = _start.transform.position;
+        from.y = 0f;
+        Vector3 to = hit.collider.transform.position;
+        to.y = 0f;
+        return Vector3.Distance(from, to);
+    }
+}
---
+++ WheelDistance.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+public class WheelDistance : Deduction
+{
+    [SerializeField] 
+    private DistanceSensor[] _sensors;
+    [SerializeField]
+    private GameObject _left_debuging_sensor;
+    [SerializeField]
+    private GameObject _right_debuging_sensor;
+    private float _whole_wheel_distance = 1.945687f; //left 1.116196 + right 0.829491
+    private float _track_distance = 4.265f;
+
+    private float persent = 0f;
+    
+    [SerializeField]
+    public Text distanceText;
+   private void Awake()
+    {
+        for(int i = 0; i<_sensors.Length; i++)
+        {
+            _sensors[i].GetComponent<DistanceSensor>();
+        }
+    }
+
+    private void Start()
+    {
+        for(int i = 0; i<_sensors.Length; i++)
+        {
+            _sensors[i].GetComponent<DistanceSensor>().ray_hit_event.AddListener(CalculateAverage);
+            _sensors[i].distance_list.Clear();
+            _sensors[i].gameObject.SetActive(true);
+        }
+        distanceText.text = "평균값 : " + persent+ "%";
+    }
+    public override void DoTest()
+    {
+        if(persent <5)
+            deductionScore = 5;
+    }
+
+    private void DebugEachDistance()
+    {
+        Debug.Log(Vector3.Distance(_left_debuging_sensor.transform.position,_right_debuging_sensor.transform.position));
+        int layer_mask = 1 << LayerMask.NameToLayer("Wheel");
+        RaycastHit hit;
+        if(Physics.Raycast(_left_debuging_sensor.transform.position, _right_debuging_sensor.transform.position-_left_debuging_sensor.transform.position,out hit,100f,layer_mask))
+        {
+            if (hit.collider.CompareTag("LeftWheel"))
+            {
+                Vector3 from = hit.collider.gameObject.transform.position;
+                from.y = 0;
+                Vector3 to = _left_debuging_sensor.transform.position;
+                to.y = 0;
+                Debug.Log("Left : "+Vector3.Distance(from,to));
+            }
+        } 
+        
+        if(Physics.Raycast(_right_debuging_sensor.transform.position, _left_debuging_sensor.transform.position-_right_debuging_sensor.transform.position,out hit,100f,layer_mask))
+        {
+            if (hit.collider.CompareTag("RightWheel"))
+            {
+                Vector3 from = hit.collider.gameObject.transform.position;
+                from.y = 0;
+                Vector3 to = _right_debuging_sensor.transform.position;
+                to.y = 0;
+                Debug.Log("Right : "+Vector3.Distance(from,to));
+            }
+        }
+
+    }
+    public void CalculateAverage()
+    {
+        float sum = 0;
+        int count = 0;
+        foreach (var i in _sensors)
+        {
+            for (int j = 0; j < i.distance_list.Count; j++)
+            {
+                float left = i.distance_list[j];
+                float right = _whole_wheel_distance - left;
+                Debug.Log(left);
+                Debug.Log(right);
+                float distance = left < right ? left : right;
+
+                Debug.Log(distance);
+                sum += distance;
+                count++;
+            }
+        }
+        Debug.Log("SUM :" + sum + " Count :" + count + " 평균값 : " + persent+ "%");
+        persent = (sum/count)/_track_distance *100;
+        distanceText.text = "평균값 : " + persent+ "%";
+    }
+}
