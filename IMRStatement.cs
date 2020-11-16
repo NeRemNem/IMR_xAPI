9534e2b597890f1752410c6dc0ec6c2587c116de
SASARA <hhc940419@gmail.com>
2020-11-16 09:34:04 +0900

    첫 커밋(파일추가)

---
+++ IMRStatement.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TinCan;
+using TinCan.LRSResponses;
+using System;
+using Newtonsoft.Json.Linq;
+
+namespace IMR
+{
+    public static class IMRStatement
+    {
+        private static Agent _actor;
+        private static Verb _verb;
+        private static Activity _activity;
+        private static ActivityDefinition _definition;
+        private static Result _result;
+        private static DateTime? _timestamp;
+        private static Agent _authority;
+        private static bool _is_init = false;
+
+        public static void Init()
+        {
+            if (_is_init == false)
+            {
+                _is_init = true;
+                _actor = new Agent();
+                _verb = new Verb();
+                _activity = new Activity();
+                _definition = new ActivityDefinition();
+                _result = new Result();
+                _timestamp = null;
+                _authority = new Agent();
+                SetAuthority();   
+
+            }
+        }
+
+        public static void SetActor(string actor = "korea_tech")
+        {
+            Init();
+            _actor.mbox = "mailto:" + actor.Replace(" ", "") + "@google.com";
+            _actor.name = actor;
+        }
+
+        public static void SetVerb(string verb)
+        {
+            Init();
+            _verb.id = new Uri("http://imrxapi.com/" + verb.Replace(" ", ""));
+            _verb.display = new LanguageMap();
+            _verb.display.Add("en", verb);
+        }
+
+        public static void SetActivity(string activity)
+        {
+            Init();
+            _activity.id = new Uri("https://www.koreatech.ac.kr/" + activity.Replace(" ", "")).ToString();
+        }
+
+        public static void SetDefinition(string def)
+        {
+            Init();
+            _definition.description = new LanguageMap();
+            _definition.name = new LanguageMap();
+            _definition.name.Add("en", (def));
+            _activity.definition = _definition;
+        }
+
+        public static void SetScore(int value)
+        {
+            _result.score.raw = value;
+        }
+
+        public static void SetTimestamp(DateTime t)
+        {
+            _timestamp = t;
+        }
+
+        public static void SetAuthority()
+        {
+            _authority.name = "KoreaTech";
+            _authority.mbox = "mailto:KoreaTech@koreatech.ac.kr";
+        }
+
+        public static Statement GetStatement()
+        {
+            Statement statement = new Statement();
+            statement.actor = _actor;
+            statement.verb = _verb;
+            statement.target = _activity;
+            statement.result = _result;
+            statement.timestamp = DateTime.Now;
+            statement.authority = _authority;
+            
+            return statement;
+        }
+    }
+}
---
+++ StatementSender.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TinCan;
+using TinCan.LRSResponses;
+using System;
+using Newtonsoft.Json.Linq;
+
+namespace IMR
+{
+    public static class StatementSender
+    {
+        private static string endpoint = null;
+        private static string key = "fbaabcea7a969befabfb7a4b7cf11b59a007d129";
+        private static string secret_key = "5af60b9abcaef1eb2cc68d191c634fccaaee0688";
+    
+        public static string debug_msg = "";
+        public static StatementLRSResponse lrs_res = null;
+    
+        private static RemoteLRS lrs;
+
+        public static void Init()
+        {
+            lrs = new RemoteLRS(endpoint,key,secret_key);
+        }
+
+        public static string EndPoint
+        {
+            get => endpoint;
+            set => endpoint = value;
+        }
+        public static string UserName
+        {
+            get => key;
+            set => key = value;
+        }
+        public static string Password
+        {
+            get => secret_key;
+            set => secret_key = value;
+        }
+        public static void SendStatement()
+        {
+            if (endpoint != null)
+            {
+                Init();
+                lrs_res = lrs.SaveStatement(IMRStatement.GetStatement());
+                if (lrs_res.success) //Success
+                {
+                    debug_msg = "Save statement: " + lrs_res.content.id;
+                    Debug.Log(debug_msg);
+                }
+                else //Failure
+                {
+                    debug_msg = "Statement Failed: " + lrs_res.errMsg;
+                    Debug.Log(debug_msg);
+                }
+            }
+        }
+    }
+
+}
